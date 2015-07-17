0bc45498e36a087d5e0a1809f1d5d064796fdf0e
Nate Wright <natew@notthisway.com>
2015-07-17 14:07:11 +0100

    * Version 0.1

--- dist/fuzzy-finder-wp.php
+++ dist/fuzzy-finder-wp.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Fuzzy Finder
  * Plugin URI: https://github.com/NateWr
- * Description: Fast fuzzy finder for your WordPress admin.
+ * Description: A fuzzy finder for your WordPress admin.
  * Version: 0.1.0
  * Author: Nate Wright
  * Author URI: http://twitter.com/NateWr
@@ -30,18 +30,45 @@ class ffwpInit {
 
 	/**
 	 * The single instance of this class
+	 *
+	 * @since 0.1
 	 */
 	private static $instance;
 
 	/**
 	 * Path to the plugin directory
+	 *
+	 * @since 0.1
 	 */
 	static $plugin_dir;
 
 	/**
 	 * URL to the plugin
+	 *
+	 * @since 0.1
 	 */
 	static $plugin_url;
+
+	/**
+	 * Version of the plugin
+	 *
+	 * @since 0.1
+	 */
+	static $plugin_version;
+
+	/**
+	 * Array of strings for search matches
+	 *
+	 * @since 0.1
+	 */
+	public $strings;
+
+	/**
+	 * Array of URLs which match the self::$strings array
+	 *
+	 * @since 0.1
+	 */
+	public $urls;
 
 	/**
 	 * Create or retrieve the single instance of the class
@@ -68,19 +95,115 @@ class ffwpInit {
 	 */
 	public function init() {
 
+		if ( !current_user_can( 'manage_options' ) ) {
+			return;
+		}
+
 		// Initialize the plugin
-		add_action( 'init', array( $this, 'load_textdomain' ) );
+		add_action( 'admin_init', array( $this, 'load_textdomain' ) );
+		add_action( 'admin_init', array( $this, 'load_config' ) );
+
+		add_action( 'admin_enqueue_scripts', array( $this, 'register_assets' ) );
+
+		// Load search data and pass finder to the frontend
+		add_action( 'admin_footer', array( $this, 'load_finder' ) );
 
 	}
 
 	/**
 	 * Load the plugin textdomain for localistion
-	 * @since 0.1.0
+	 *
+	 * @since 0.1
 	 */
 	public function load_textdomain() {
 		load_plugin_textdomain( 'fuzzy-finder-wp', false, plugin_basename( dirname( __FILE__ ) ) . '/languages/' );
 	}
 
+	/**
+	 * Load the plugin's configuration variables
+	 *
+	 * @since 0.1
+	 */
+	public function load_config() {
+		$data = get_plugin_data( __FILE__, false, false );
+		self::$plugin_version = $data['Version'];
+	}
+
+	/**
+	 * Register the script and style assets
+	 *
+	 * @since 0.1
+	 */
+	public function register_assets() {
+		wp_enqueue_style( 'ffwp_finder', self::$plugin_url . '/assets/css/finder.css', '', self::$plugin_version );
+		wp_enqueue_script( 'ffwp_finder', self::$plugin_url . '/assets/js/finder.js', array( 'jquery' ), self::$plugin_version, true );
+	}
+
+	/**
+	 * Load search data and pass finder to the frontend
+	 *
+	 * @since 0.1
+	 */
+	public function load_finder() {
+
+		$this->get_menu_strings();
+
+		// Compile a template for a result
+		ob_start();
+		include( self::$plugin_dir . '/templates/result.php' );
+		$result_template = ob_get_clean();
+
+		// Pass data
+		wp_localize_script(
+			'ffwp_finder',
+			'ffwp_finder_settings',
+			array(
+				'strings' => $this->strings,
+				'urls' => $this->urls,
+				'result_template' => $result_template,
+			)
+		);
+
+		// Print modal template markup
+		include( self::$plugin_dir . '/templates/finder.php' );
+	}
+
+	/**
+	 * Retrieve all menu strings
+	 *
+	 * @since 0.1
+	 */
+	public function get_menu_strings() {
+		global $menu;
+		global $submenu;
+
+		foreach( $menu as $item ) {
+
+			// Skip separators
+			if ( empty( $item[0] ) ) {
+				continue;
+			}
+
+			$this->strings[] = $item[0];
+			$this->urls[] = $this->get_menu_item_url( $item[2] );
+
+			if ( !empty( $submenu[ $item[2] ] ) ) {
+				foreach( $submenu[ $item[2] ] as $subitem ) {
+					$this->strings[] = $item[0] . apply_filters( 'ffwp_string_separator', ' > ' ) . $subitem[0];
+					$this->urls[] = $this->get_menu_item_url( $subitem[2] );
+				}
+			}
+		}
+	}
+
+	/**
+	 * Retrieve a URL for a menu item
+	 *
+	 * @since 0.1
+	 */
+	public function get_menu_item_url( $slug ) {
+		return strpos( $slug, '.php' ) === false ? menu_page_url( $slug, false ) : get_admin_url( null, $slug );
+	}
 }
 } // endif;
 
---
+++ dist/templates/result.php
@@ -0,0 +1,5 @@
+<li id="ffwp-result-{index}">
+	<a href="{url}">
+		<span class="string">{string}</span>
+	</a>
+</li>
