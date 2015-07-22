a43e48fe31d38920cf53572753955f7162865b2b
Nate Wright <natew@notthisway.com>
2015-07-22 16:57:52 +0100

    * Add markup for status bar (not implemented)

--- src/templates/finder.php
+++ src/templates/finder.php
@@ -7,5 +7,19 @@
 			<input type="text" name="ffwp-search" id="ffwp-search" placeholder="<?php _e( 'Search' ); ?>">
 		</div>
 		<ul class="ffwp-results"></ul>
+		<div class="ffwp-status waiting">
+			<span class="ffwp-waiting">
+				<?php esc_html_e( 'Waiting for input', 'fuzzy-finder-wp' ); ?>
+			</span>
+			<span class="ffwp-fetching">
+				<?php esc_html_e( 'Fetching symbols', 'fuzzy-finder-wp' ); ?>
+			</span>
+			<span class="ffwp-searching">
+				<?php esc_html_e( 'Searching', 'fuzzy-finder-wp' ); ?>
+			</span>
+			<span class="ffwp-complete">
+				<?php esc_html_e( 'Finished', 'fuzzy-finder-wp' ); ?>
+			</span>
+		</div>
 	</div>
 </div>
