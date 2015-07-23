dd7391d6096fb9d034fe3fe1bca17276d12355aa
Nate Wright <natew@notthisway.com>
2015-07-23 22:05:17 +0100

    * Add status icons and restructure LESS into multiple files

--- src/templates/finder.php
+++ src/templates/finder.php
@@ -9,15 +9,19 @@
 		<ul class="ffwp-results"></ul>
 		<div class="ffwp-status waiting">
 			<span class="ffwp-waiting">
+				<span class="dashicons dashicons-arrow-right"></span>
 				<?php esc_html_e( 'Waiting for 3 characters', 'fuzzy-finder-wp' ); ?>
 			</span>
 				<span class="ffwp-fetching">
+					<span class="ffwp-spinner"></span>
 					<?php esc_html_e( 'Fetching symbols', 'fuzzy-finder-wp' ); ?>
 				</span>
 			<span class="ffwp-searching">
+				<span class="ffwp-spinner"></span>
 				<?php esc_html_e( 'Searching', 'fuzzy-finder-wp' ); ?>
 			</span>
 			<span class="ffwp-complete">
+				<span class="dashicons dashicons-yes"></span>
 				<?php esc_html_e( 'Finished', 'fuzzy-finder-wp' ); ?>
 			</span>
 		</div>
